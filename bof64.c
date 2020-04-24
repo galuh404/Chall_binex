21f56388f892856f0a3d6baa69c2a76840912a68
GaluhMuhammadImanAkbar <galuh.id1234@gmail.com>
2020-04-24 23:12:48 +0700

    binex

---
+++ bof64.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+  int b = -1;
+  char c[20];
+
+  gets(&c);
+
+  if(strlen(c) != 10){
+    printf("Error: Panjang data harus 10 Yeeee\n");
+  }else{
+    if (b == 0xdeadf00d){
+      printf("Onichan BAKA!!! >:|\n");
+      system("cat flag.txt");
+    }else{
+      printf("Nope >.< \n");
+    }
+  }
+  return 0;
+}
